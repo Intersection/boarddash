91a64a827171613d134146b08474e0395af0d588
Gordon Diggs <gordon@gordondiggs.com>
2018-09-25 17:10:46 -0400

    Add sample community board data

--- home/management/commands/seed_boards.py
+++ home/management/commands/seed_boards.py
@@ -1,15 +1,26 @@
+import csv
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
+from django.conf import settings
 
+from home.models import CommunityBoard
+
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'boards.csv')
 
 class Command(BaseCommand):
     help = 'Seeds the database with community board information'
 
     def handle(self, *args, **options):
         try:
-            # Load data file
-            # Remove all community boards
-            # Add community boards from file
+            with open(filename) as f:
+                for row in csv.DictReader(f, skipinitialspace=True):
+                    model_record = CommunityBoard()
+                    for key, value in row.items():
+                        setattr(model_record, key, value)
+                    model_record.save()
             self.stdout.write(self.style.SUCCESS('Successfully executed statements'))
         except Exception as e:
             self.stderr.write(e)
