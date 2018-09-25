a573cfdb613287862422b2b1da2f3f655c45a94a
Tyler Green <tyler.green@intersection.com>
2018-09-25 15:12:26 -0400

    Operate on each record

--- home/management/commands/seed_db.py
+++ home/management/commands/seed_db.py
@@ -15,11 +15,15 @@ class Command(BaseCommand):
                 'format': 'json',
                 'community_board': '01 MANHATTAN',
                 'limit': 2,
-                'offset': 14500,
+                'offset': 14100,
             }
             url = "https://data.cityofnewyork.us/resource/{dataset_identifier}.{format}?community_board={community_board}&$limit={limit}&$offset={offset}&$order=:id&$where=created_date > '2018-01-01T00:00:00.0000'".format(**url_args)
             response = requests.get(url, timeout=settings.REQUESTS_TIMEOUT_SECONDS)
-            print(response.json())
+
+            for record in response.json():
+                print('RECORD')
+                print(record)
+                # Create 311Record object here
             self.stdout.write(self.style.SUCCESS('Successfully executed statements'))
         except Exception as e:
             self.stderr.write(e)
