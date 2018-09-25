ddcd0fe28e36468b33b1c80e7da1cabab24f5d19
Tyler Green <tyler.green@intersection.com>
2018-09-25 17:26:03 -0400

    Add simple board page with top requests by category

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Count
+
+from home.models import NYC311Record
+
 
 def index(request):
     data = {
@@ -9,3 +13,10 @@ def index(request):
         'staten_island_boards': range(1, 4),
     }
     return render(request, 'index.html', data)
+
+def board_detail(request):
+    category_dict = NYC311Record.objects.values('complaint_type').annotate(total=Count('complaint_type')).order_by('-total')
+    data = {
+            'categories': list(category_dict)[:5],
+    }
+    return render(request, 'board_detail.html', data)
