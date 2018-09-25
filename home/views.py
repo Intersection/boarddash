b1ddca6888328496744dd55fbc8f57675ff0a2d0
Tyler Green <tyler.green@intersection.com>
2018-09-25 19:18:27 -0400

    Spit budget requests out to board_detail

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count
 
-from home.models import NYC311Record, CommunityBoard
+from home.models import NYC311Record, CommunityBoard, BudgetRequest
 
 
 def index(request):
@@ -17,8 +17,12 @@ def index(request):
 def board_detail(request, **kwargs):
     community_board = CommunityBoard.objects.get(slug=kwargs['board_slug'])
     category_dict = NYC311Record.objects.filter(community_board_relation=community_board).values('complaint_type').annotate(total=Count('complaint_type')).order_by('-total')
+    capital_requests = BudgetRequest.objects.filter(community_board_relation=community_board, priority='CS')
+    operational_requests = BudgetRequest.objects.filter(community_board_relation=community_board).exclude(priority='CS')
     data = {
             'community_board': community_board,
             'categories': list(category_dict)[:5],
+            'budget_requests_capital': capital_requests[:5],
+            'budget_requests_operational': operational_requests[:5],
     }
     return render(request, 'board_detail.html', data)
