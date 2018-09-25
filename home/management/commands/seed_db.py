ddaefa37b5ff1d4b8e43791d40c7d8a62c949588
Tyler Green <tyler.green@intersection.com>
2018-09-25 15:08:22 -0400

    Initial request in seed_db

--- home/management/commands/seed_db.py
+++ home/management/commands/seed_db.py
@@ -1,5 +1,8 @@
+import requests
+
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -7,7 +10,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            # Hit API here
+            url_args = {
+                'dataset_identifier': 'fhrw-4uyv',
+                'format': 'json',
+                'community_board': '01 MANHATTAN',
+            }
+            url = "https://data.cityofnewyork.us/resource/{dataset_identifier}.{format}?community_board={community_board}&$limit=2&$offset=14100&$order=:id&$where=created_date > '2018-01-01T00:00:00.0000'".format(**url_args)
+            response = requests.get(url, timeout=settings.REQUESTS_TIMEOUT_SECONDS)
+            print(response.json())
             self.stdout.write(self.style.SUCCESS('Successfully executed statements'))
         except Exception as e:
             self.stderr.write(e)
