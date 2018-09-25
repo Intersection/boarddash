5f60fee4ce4a242b7e26321e7b330fb28c98f244
Tyler Green <tyler.green@intersection.com>
2018-09-25 17:28:37 -0400

    Add CommunityBoard to admin

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import NYC311Record
+from .models import NYC311Record, CommunityBoard
 
 admin.site.register(NYC311Record)
+admin.site.register(CommunityBoard)
--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('^$', views.index, name='index'),
+    path('', views.index, name='index'),
     #path('boards/<board_id>$', views.board_detail, name='board_detail'),
     path(r'boards', views.board_detail, name='board_detail'),
 ]
